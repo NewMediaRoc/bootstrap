d3f61c0483361e6f6a667ab56fabfe37d0e82fec
J. Bruni <contato@jbruni.com.br>
2013-10-06 21:47:28 -0300

    Made popover-template directive compatible with AngularJS 1.0.8 Removed jQuery dependency Accepts external file templates

--- src/popover/popover.js
+++ src/popover/popover.js
@@ -15,15 +15,22 @@ angular.module( 'ui.bootstrap.popover', [ 'ui.bootstrap.tooltip' ] )
 .directive( 'popover', [ '$compile', '$timeout', '$parse', '$window', '$tooltip', function ( $compile, $timeout, $parse, $window, $tooltip ) {
   return $tooltip( 'popover', 'popover', 'click' );
 }])
-.directive( 'popoverTemplatePopup', [ '$templateCache', '$compile', function ( $templateCache, $compile ) {
+.directive( 'popoverTemplatePopup', [ '$http', '$templateCache', '$compile', function ( $http, $templateCache, $compile ) {
   return {
     restrict: 'EA',
     replace: true,
     scope: { title: '@', content: '@', placement: '@', animation: '&', isOpen: '&', compileScope: '&' },
     templateUrl: 'template/popover/popover-template.html',
     link: function( scope, iElement ) {
-      iElement.find( 'div.popover-content' )
-      .html( $compile( $templateCache.get( scope.content ) )( scope.compileScope() ) );
+      scope.$watch( 'content', function( templateUrl ) {
+        if ( !templateUrl ) return;
+        $http.get( templateUrl, { cache: $templateCache } )
+        .then( function( response ) {
+          var contentEl = angular.element( iElement[0].querySelector( '.popover-content' ) );
+          contentEl.children().remove();
+          contentEl.append( $compile( response.data.trim() )( scope.compileScope() ) );
+        });
+      });
     }
   };
 }])
